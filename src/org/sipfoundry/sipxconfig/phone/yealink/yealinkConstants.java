214322698941087aa6b0ffd51a30405e752c168e
siplabs <kvishnivetsky@siplabs.ru>
2013-02-19 12:31:45 +0700

    ADD: Some sections moveed. JAVA code updated for new settings location.

--- src/org/sipfoundry/sipxconfig/phone/yealink/yealinkConstants.java
+++ src/org/sipfoundry/sipxconfig/phone/yealink/yealinkConstants.java
@@ -47,44 +47,44 @@ public class yealinkConstants {
     public static final String VOICE_MAIL_NUMBER_V7X_SETTING = "basic/voice_mail.number";
 
     // Phone specific settings used in /etc/yealinkPhone/phone.xml
-    public static final String DNS_SERVER1_V6X_SETTING = "DNS/PrimaryDNS";
+    public static final String DNS_SERVER1_V6X_SETTING = "network-wan/DNS/PrimaryDNS";
     public static final String DNS_SERVER1_V7X_SETTING = "network/network.primary_dns";
-    public static final String DNS_SERVER2_V6X_SETTING = "DNS/SecondaryDNS";
+    public static final String DNS_SERVER2_V6X_SETTING = "network-wan/DNS/SecondaryDNS";
     public static final String DNS_SERVER2_V7X_SETTING = "network/network.secondary_dns";
-    public static final String LOCAL_TIME_SERVER1_V6X_SETTING = "Time/TimeServer1";
+    public static final String LOCAL_TIME_SERVER1_V6X_SETTING = "preference/Time/TimeServer1";
     public static final String LOCAL_TIME_SERVER1_V7X_SETTING = "time/local_time.ntp_server1";
-    public static final String LOCAL_TIME_SERVER2_V6X_SETTING = "Time/TimeServer2";
+    public static final String LOCAL_TIME_SERVER2_V6X_SETTING = "preference/Time/TimeServer2";
     public static final String LOCAL_TIME_SERVER2_V7X_SETTING = "time/local_time.ntp_server2";
-    public static final String LOCAL_TIME_ZONE_V6X_SETTING = "Time/TimeZone";
+    public static final String LOCAL_TIME_ZONE_V6X_SETTING = "preference/Time/TimeZone";
     public static final String LOCAL_TIME_ZONE_V7X_SETTING = "time/local_time.time_zone";
-    public static final String SYSLOG_SERVER_V6X_SETTING = "SYSLOG/SyslogdIP";
+    public static final String SYSLOG_SERVER_V6X_SETTING = "upgrade/SYSLOG/SyslogdIP";
     public static final String SYSLOG_SERVER_V7X_SETTING = "network/syslog.server";
     public static final String REMOTE_PHONEBOOK_0_URL_V6X_SETTING = "RemotePhoneBook/0/URL";
     public static final String REMOTE_PHONEBOOK_0_URL_V7X_SETTING = "remote-phonebook/remote_phonebook.data.1.url";
     public static final String REMOTE_PHONEBOOK_0_NAME_V6X_SETTING = "RemotePhoneBook/0/Name";
     public static final String REMOTE_PHONEBOOK_0_NAME_V7X_SETTING = "remote-phonebook/remote_phonebook.data.1.name";
-    public static final String FIRMWARE_SERVER_ADDRESS_SETTING = "firmware/server_ip";
-    public static final String FIRMWARE_URL_V6X_SETTING = "firmware/url";
+    public static final String FIRMWARE_SERVER_ADDRESS_SETTING = "upgrade/firmware/server_ip";
+    public static final String FIRMWARE_URL_V6X_SETTING = "upgrade/firmware/url";
     public static final String FIRMWARE_URL_V7X_SETTING = "downloads/firmware.url";
-    public static final String FIRMWARE_HTTP_URL_SETTING = "firmware/http_url";
-    public static final String FIRMWARE_NAME_SETTING = "firmware/firmware_name";
-    public static final String AUTOPROVISIONING_SERVER_URL_V6X_SETTING = "autoprovision/strServerURL";
+    public static final String FIRMWARE_HTTP_URL_SETTING = "upgrade/firmware/http_url";
+    public static final String FIRMWARE_NAME_SETTING = "upgrade/firmware/firmware_name";
+    public static final String AUTOPROVISIONING_SERVER_URL_V6X_SETTING = "upgrade/autoprovision/strServerURL";
     public static final String AUTOPROVISIONING_SERVER_URL_V7X_SETTING = "auto-provisioning/auto_provision.server.url";
     public static final String AUTOPROVISIONING1_SERVER_URL_V7X_SETTING = "auto-provisioning/autoprovision.1.url";
     public static final String AUTOPROVISIONING_SERVER_ADDRESS_SETTING = "autoprovision/server_address";
-    public static final String AUTOPROVISIONING1_SERVER_ADDRESS_SETTING = "autoprovision1/Server_address";
+    public static final String AUTOPROVISIONING1_SERVER_ADDRESS_SETTING = "autoprovision/1/Server_address";
     public static final String ADVANCED_MUSIC_SERVER_URI_V6X_SETTING = "account/MusicServerUri";
     public static final String ADVANCED_MUSIC_SERVER_URI_V7X_SETTING = "advanced/music_server_uri";
-    public static final String LANG_FILE_NAME_V6X_SETTING = "LangFile/server_address";
+    public static final String LANG_FILE_NAME_V6X_SETTING = "upgrade/LangFile/server_address";
     public static final String LANG_FILE_NAME_V7X_SETTING = "downloads/gui_lang.url";
 // T2X except T20
-    public static final String LOGO_FILE_NAME_V6X_SETTING = "Logo/server_address";
+    public static final String LOGO_FILE_NAME_V6X_SETTING = "upgrade/Logo/server_address";
     public static final String LOGO_FILE_NAME_V7X_SETTING = "downloads/lcd_logo.url";
 // T3X and VP530 only
-    public static final String WALLPAPER_FILE_NAME_V6X_SETTING = "Logo/server_address";
+    public static final String WALLPAPER_FILE_NAME_V6X_SETTING = "upgrade/Logo/server_address";
     public static final String WALLPAPER_FILE_NAME_V7X_SETTING = "downloads/wallpaper_upload.url";
 // T3X
-    public static final String SCREENSAVER_FILE_NAME_V6X_SETTING = "Logo/server_address";
+    public static final String SCREENSAVER_FILE_NAME_V6X_SETTING = "upgrade/Logo/server_address";
     public static final String SCREENSAVER_FILE_NAME_V7X_SETTING = "downloads/screen_saver.pic.url";
 
     public static final String DIAL_NOW_URL_V6X_SETTING = "DialNow/server_address";
