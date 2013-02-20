e6c867dec3d1dee8bf005a66e7ac4e490980303d
siplabs <kvishnivetsky@siplabs.ru>
2013-02-20 15:38:45 +0700

    Added Ru labels to all sections. Fixed isue with DialNow section. Fixed issue with DialNow URL default value.

--- src/org/sipfoundry/sipxconfig/phone/yealink/yealinkConstants.java
+++ src/org/sipfoundry/sipxconfig/phone/yealink/yealinkConstants.java
@@ -83,7 +83,7 @@ public class yealinkConstants {
 // T3X
     public static final String SCREENSAVER_FILE_NAME_V7X_SETTING = "downloads/screen_saver.pic.url";
 
-    public static final String DIAL_NOW_URL_V6X_SETTING = "DialNow/server_address";
+    public static final String DIAL_NOW_URL_V6X_SETTING = "dialplan/DialNow/server_address";
     public static final String DIAL_NOW_URL_V7X_SETTING = "downloads/dialplan_dialnow.url";
 
 
